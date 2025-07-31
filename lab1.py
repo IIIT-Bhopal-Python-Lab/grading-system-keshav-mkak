fe01160601ac0b883609c07f76bbcd2e5a1bb37b
keshav-mkak <keshav.mkak1907@gmail.com>
2025-07-31 09:57:54 +0530

    Submit lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,12 @@
-#Write your code here in python 
-n=int(input("enter a nuber"))
-print(n)
+while True:
+    i = input()
+    if i.upper() == "EXIT":
+        break
+    try:
+        s = float(i)
+        if 90 <= s <= 100: print("A")
+        elif 75 <= s < 90: print("B")
+        elif 60 <= s < 75: print("C")
+        elif 40 <= s < 60: print("D")
+        elif 0 <= s < 40: print("F")
+    except: pass
