db4f3ebef47ac94c4b0b915e15abef8056f12eda
keshav-mkak <keshav.mkak1907@gmail.com>
2025-07-31 10:03:57 +0530

    Submit lab1.py

--- lab1.py
+++ lab1.py
@@ -1,12 +1,25 @@
-while True:
-    i = input()
+import sys
+#Handling the case where we have to EXIT the program
+for line in sys.stdin:
+    i = line.strip()
     if i.upper() == "EXIT":
         break
     try:
+    #Taking marks as an Input to Calculate Grades 
         s = float(i)
-        if 90 <= s <= 100: print("A")
-        elif 75 <= s < 90: print("B")
-        elif 60 <= s < 75: print("C")
-        elif 40 <= s < 60: print("D")
-        elif 0 <= s < 40: print("F")
-    except: pass
+        if 0 <= s <= 100:
+            if 90 <= s <= 100:
+                print("A")
+            elif 75 <= s < 90:
+                print("B")
+            elif 60 <= s < 75:
+                print("C")
+            elif 40 <= s < 60:
+                print("D")
+            else:
+                print("F")
+        else:
+        # Handling the case of Invalid Input 
+            print("Invalid Input")
+    except:
+        print("Invalid Input")
